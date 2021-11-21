42928cb943173eeef1065f71f5fe8831b98f8bac
mobin <mobinsh_20@yahoo.com>
2021-11-21 19:13:09 +0330

    Add : Platform and player Move

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject platformprefab;
+    public int platformcount = 300;
+    
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+        for (int i = 0; i < platformcount; i++)
+        {
+            spawnPosition.y += Random.Range(.5f, 2f);
+            spawnPosition.x = Random.Range(-5f, 5f);
+            Instantiate(platformprefab, spawnPosition, Quaternion.identity);
+            
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+    private float moveX;
+    
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = moveX;
+        rb.velocity = velocity;
+        
+    }
+}
